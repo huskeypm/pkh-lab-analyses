75f81862d735064017add3e2edd7a0282a1918db
bsu233 <sunbinxod@gmail.com>
2021-07-19 15:14:56 -0500

    added sCaM scripts

---
+++ 2021-sCaM/homology-modeling/soyCaM4_modeling/rosetta_cm/run2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rosetta_scripts.linuxgccrelease @flags -database /home/AD/bsu233/Apps/rosetta_src_2016.15.58628_bundle/main/database -nstruct 10
---
+++ 2021-sCaM/sketch-map/step2_landmark.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+dimlandmark -D 32 -n 4000 -pi -mode minmax -w -lowmem < hddata > landmark
